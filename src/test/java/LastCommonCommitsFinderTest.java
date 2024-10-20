76dd24ac8545b3d9d229cfd2472748f4d9be15a4
Casian Sebastian Chiriac <S.C.Chiriac@student.tudelft.nl>
2024-10-20 18:06:38 +0200

    added better testing and javadoc and some better error handling

--- src/test/java/LastCommonCommitsFinderTest.java
+++ src/test/java/LastCommonCommitsFinderTest.java
@@ -1,16 +1,27 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.Properties;
 
 public class LastCommonCommitsFinderTest {
+    private String getPatToken() throws IOException { /// PAT token is a secret and needs to be kept in a .ignore to commit changes
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("src/main/resources/application.properties")) {
+            properties.load(input);
+        }
+        return properties.getProperty("PAT_TOKEN");
+    }
+
     @Test
     /// https://github.com/ChiriacCasian/RestApiTemplate/network
     public void TestLastCommonCommitsFinderOnRestApiTemplate(){
         try {
+            String patToken = getPatToken();
         LastCommonCommitsFinder lastCommonCommitsFinder = (new LastCommonCommitsFinderFactoryImpl())
-                .create("ChiriacCasian", "RestApiTemplate", "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") ;
+                .create("ChiriacCasian", "RestApiTemplate", patToken) ;
             Assert.assertEquals(lastCommonCommitsFinder.findLastCommonCommits("master", "AltMaster"), Collections.singletonList("7e84212aa23a39110f567c7ee1683f19683f0863"));
             Assert.assertEquals(lastCommonCommitsFinder.findLastCommonCommits("master", "new-remote-branch"), Collections.singletonList("7e84212aa23a39110f567c7ee1683f19683f0863"));
 
